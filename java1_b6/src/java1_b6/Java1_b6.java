618667ab6a798c8de1e521bfac20ad6cd7d5ff72
PH39919HH <127483245+PH39919HH@users.noreply.github.com>
2025-01-21 14:00:48 +0700

    b6

---
+++ java1_b6/src/java1_b6/Java1_b6.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package java1_b6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class Java1_b6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("moi may nhap:");
+        int n = Integer.valueOf(sc.nextLine());
+        int arr[] = new int[n];
+        System.out.print("nhap cac mang phan tu:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.valueOf(sc.nextLine());
+        }
+        System.out.println("in cac phan tu trong mang");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
